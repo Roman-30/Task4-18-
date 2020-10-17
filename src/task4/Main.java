1f349a1f71f0563a1bed3e303c495c4b6d1b7966
Roman Shevtsov <Romses2016@yandex.ru>
2020-10-18 00:40:52 +0300

    task4

--- src/task4/Main.java
+++ src/task4/Main.java
@@ -4,20 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         int number = 100;
-        int quantity = iterateovernumbers(number);
-        printanswer(quantity);
+        int quantity = iterateOverNumbers(number);
+
+        printAnswer(quantity);
     }
 
-    public static int iterateovernumbers(int number) {
+    public static int iterateOverNumbers(int number) {
         int quantity = 0;
         while (number <= 999) {
             int firstdigit = number % 10;
             int seconddigit = (number / 10) % 10;
             int thirddigit = (number / 100) % 10;
-            if ((seconddigit - firstdigit >= 3 && thirddigit - seconddigit > 3) ||
-                    (firstdigit - seconddigit >= 3 && seconddigit - thirddigit > 3) ||
-                    (seconddigit - firstdigit >= 3 && seconddigit - thirddigit >= 3) ||
-                    (firstdigit - seconddigit >= 3 && thirddigit - seconddigit >= 3)) {
+
+            if ( establishCondition(firstdigit, seconddigit,thirddigit)) {
                 quantity += 1;
                 number++;
             } else {
@@ -27,14 +26,15 @@ public class Main {
         return quantity;
     }
 
-    public static void printanswer(int quantity){
-        if (quantity > 0){
-            System.out.println("The number of numbers, differing by three digit: " + quantity);
-        } else {
-            System.out.println("Error");
-        }
+    static boolean  establishCondition(int firstdigit, int seconddigit, int thirddigit) {
+        return (seconddigit - firstdigit >= 3 && thirddigit - seconddigit > 3) ||
+                (firstdigit - seconddigit >= 3 && seconddigit - thirddigit > 3) ||
+                (seconddigit - firstdigit >= 3 && seconddigit - thirddigit >= 3) ||
+                (firstdigit - seconddigit >= 3 && thirddigit - seconddigit >= 3);
     }
 
-
+    public static void printAnswer(int quantity) {
+            System.out.println("The number of numbers, differing by three digit: " + quantity);
+    }
 }
 
