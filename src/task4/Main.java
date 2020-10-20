967fbd33230282547c260af2b2826e36341424e2
Roman Shevtsov <Romses2016@yandex.ru>
2020-10-20 19:19:05 +0300

    task4

--- src/task4/Main.java
+++ src/task4/Main.java
@@ -4,20 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         int number = 100;
-        int quantity = chooseAndCountNumbers(number);
+        int quantity = calculateQuantityNumbers(number);
 
         printAnswer(quantity);
     }
 
-    public static  int chooseAndCountNumbers(int number) {
+    public static  int calculateQuantityNumbers(int number) {
         int quantity = 0;
 
-        while (number <= 999 & number >= 100) {
+        while (number >= 100 & number <= 999) {
             int firstDigit = number % 10;
             int secondDigit = (number / 10) % 10;
             int thirdDigit = (number / 100) % 10;
 
-            if ( establishCondition(firstDigit, secondDigit, thirdDigit)) {
+            if ( mustDo(firstDigit, secondDigit, thirdDigit)) {
                 quantity += 1;
                 number++;
 
@@ -28,7 +28,7 @@ public class Main {
         return quantity;
     }
 
-    static boolean  establishCondition(int firstDigit, int secondDigit, int thirdDigit) {
+    static boolean  mustDo(int firstDigit, int secondDigit, int thirdDigit) {
         return (secondDigit - firstDigit >= 3 && thirdDigit - secondDigit > 3) ||
                 (firstDigit - secondDigit >= 3 && secondDigit - thirdDigit > 3) ||
                 (secondDigit - firstDigit >= 3 && secondDigit - thirdDigit >= 3) ||
